4764ac646bc6d6945793e006c63c752e5184ea0e
andresjimenito-cell <andresjimenito@gmail.com>
2026-06-05 10:14:06 -0500

    Agregar columna Fallas ALS < 1500

--- tabs/tab_indices.py
+++ tabs/tab_indices.py
@@ -167,6 +167,7 @@ def render_tab_indices(df_bd_filtered, df_forma9_filtered, fecha_evaluacion, sel
             df_detalle['Pozos On'] = df_mensual_hist['Pozos ON'].fillna(0).astype(int)
             df_detalle['Pozos Fallados'] = df_mensual_hist['Fallas Totales'].fillna(0).astype(int)
             df_detalle['Fallas < 1500'] = df_mensual_hist.get('Fallas_1500', pd.Series([0]*len(df_mensual_hist))).fillna(0).astype(int)
+            df_detalle['Fallas ALS < 1500'] = df_mensual_hist.get('Fallas_ALS_1500', pd.Series([0]*len(df_mensual_hist))).fillna(0).astype(int)
             df_detalle['Fallas ALS'] = df_mensual_hist['Fallas ALS'].fillna(0).astype(int)
             df_detalle['Pozos Off'] = (df_mensual_hist['Pozos Operativos'] - df_mensual_hist['Pozos ON']).clip(lower=0).fillna(0).astype(int)
             
